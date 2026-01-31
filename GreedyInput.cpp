e4d831e03b2497ba7d98f6894c0e5e7f993ba880
Jamiul <jamiulsunny476@gmail.com>
2026-01-31 18:20:37 +0600

    Add files via upload

---
+++ GreedyInput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ int main(){
+     string firstName,lastName;
+cout<<"Enter the first and last Name : ";
+
+cin >> firstName >>lastName;
+
+cout<<"Name: "<<firstName<<" "<<lastName << endl;
+
+return 0;
+
+ }
+ 
