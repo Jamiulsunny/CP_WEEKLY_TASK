433ab3a6557bc9c9306fd43e82d83d8a1ee551fd
Jamiul <jamiulsunny476@gmail.com>
2026-01-31 18:20:16 +0600

    Add files via upload

---
+++ immortal_Player.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void takeDamage ( int &health, int damage) {
+
+health -=damage ;
+
+}
+ int main() {
+int playerHP = 100 ;
+
+
+takeDamage( playerHP , 30);
+takeDamage( playerHP,  20);
+
+cout << " Final HP: " <<playerHP << endl;
+ return 0 ;
+
+ }
